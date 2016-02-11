b75f7b85d3c810cdcbd5376ada6ed3d27a44042a
Jordan Meier and Mary Warrington <marywarrington@gmail.com>
2016-02-11 13:44:04 -0800

    instantiate new Artist and Cd objects and save them in /cds route

--- app/app.php
+++ app/app.php
@@ -1,10 +1,15 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Cd.php";
+    require_once __DIR__."/../src/Artist.php";
+
 
     session_start();
         if (empty($_SESSION['list_of_cds'])) {
             $_SESSION['list_of_cds'] = array();
+        }
+        if (empty($_SESSION['list_of_artists'])) {
+            $_SESSION['list_of_artists'] = array();
         }
 
     $app = new Silex\Application();
@@ -15,12 +20,15 @@
     // routes
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('cds.html.twig', array('cds' => Cd::getAll()));
+        return $app['twig']->render('cds.html.twig', array('cds' => Cd::getAll()), array('artists' => Artist::getAll()));
     });
 
     $app->post("/cds", function() use ($app) {
-        $new_cd = new Cd($_POST['artist'], $_POST['title'], $_POST['cover']);
-        $new_cd->save();
+
+        $new_artist = new Artist($_POST['artist']);
+        $new_cd = new Cd($new_artist, $_POST['title'], $_POST['cover']);
+        $new_cd->saveCd();
+        $new_artist->saveArtist();
 
         return $app['twig']->render('cds.html.twig', array('cds' => Cd::getAll()));
     });
