54b487b41749706bf88cd4c1e85ffd3ac3e4fc24
Jordan Meier and Mary Warrington <marywarrington@gmail.com>
2016-02-11 11:50:11 -0800

    add home, cds and delete_cds routes

--- app/app.php
+++ app/app.php
@@ -1,10 +1,33 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Cd.php";
+
+    session_start();
+        if (empty($_SESSION['list_of_cds'])) {
+            $_SESSION['list_of_cds'] = array();
+        }
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
-    $app->get("/", function() {
-        return "Home";
+    // routes
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('cds.html.twig', array('cds' => Cd::getAll()));
+    });
+
+    $app->post("/cds", function() use ($app) {
+        $new_cd = new Cd($_POST['artist'], $_POST['title'], $_POST['cover']);
+        $new_cd->save();
+
+        return $app['twig']->render('cds.html.twig', array('cds' => Cd::getAll()));
+    });
+
+    $app->post("/delete_cds", function() use ($app) {
+        Cd::deleteAll();
+        return $app['twig']->render('cds.html.twig', array('cds' => Cd::getAll()));
     });
 
     return $app;
